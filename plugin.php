2d8f8ed0f8f4d9b06ad2f461d22404067719fc30
Brandon Kraft <web-comments@brandonkraft.com>
2012-03-20 22:20:34 -0500

    Initial Code.

---
+++ plugin.php
@@ -0,0 +1,26 @@
+<?php
+/**
+ * Plugin Name: Genesis eNews Extended
+ * Plugin URI: http://www.brandonkraft.com
+ * Description: Replaces the Genesis eNews Widget to allow easier use of additional mailing services.
+ * Version: 0.1-alpha1
+ * Author: Brandon Kraft
+ * Author URI: http://www.brandonkraft.com
+ *
+ * This program is free software; you can redistribute it and/or modify it under the terms of the GNU 
+ * General Public License version 2, as published by the Free Software Foundation.  You may NOT assume 
+ * that you can use any other version of the GPL.
+ *
+ * This program is distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY; without 
+ * even the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.
+ *
+ * @package BK_Genesis_enews_extended
+ * @version 0.1-prealpha1
+ * @author Brandon Kraft <bk@kraft.im>
+ * @copyright Copyright (c) 2012, Brandon Kraft
+ * @link http://www.brandonkraft.com
+ * @license http://www.gnu.org/licenses/old-licenses/gpl-2.0.html
+ */
+
+// The actual plugin will go below once I write it. :-)
+ 
---
+++ readme.txt
@@ -0,0 +1,3 @@
+=== Genesis eNews Extended ===
+
+Creates a new widget to replace the Genesis eNews Widget to allow easier use of non-Feedburner mailing lists.
