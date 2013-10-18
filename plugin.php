62cda5f3003227aa89b0a12952126d9531184396
Gary Jones <GaryJones@users.noreply.github.com>
2013-10-18 15:40:33 +0100

    Make plugin header tags easier to read

--- plugin.php
+++ plugin.php
@@ -2,21 +2,21 @@
 /**
  * Genesis eNews Extended
  *
- * @package BJGK\Genesis_enews_extended
- * @version 1.3.3
- * @author Brandon Kraft <public@brandonkraft.com>
- * @copyright Copyright (c) 2012, Brandon Kraft
- * @link http://www.brandonkraft.com/contrib/plugins/genesis-enews-extended/
- * @license GPL-2.0+
+ * @package     BJGK\Genesis_enews_extended
+ * @version     1.3.3
+ * @author      Brandon Kraft <public@brandonkraft.com>
+ * @copyright   Copyright (c) 2012, Brandon Kraft
+ * @link        http://www.brandonkraft.com/contrib/plugins/genesis-enews-extended/
+ * @license     GPL-2.0+
  *
  * @wordpress-plugin
  * Plugin Name: Genesis eNews Extended
- * Plugin URI: http://www.brandonkraft.com/contrib/plugins/genesis-enews-extended/
+ * Plugin URI:  http://www.brandonkraft.com/contrib/plugins/genesis-enews-extended/
  * Description: Replaces the Genesis eNews Widget to allow easier use of additional mailing services.
- * Version: 1.3.3
- * Author: Brandon Kraft
- * Author URI: http://www.brandonkraft.com
- * License: GPL-2.0+
+ * Version:     1.3.3
+ * Author:      Brandon Kraft
+ * Author URI:  http://www.brandonkraft.com
+ * License:     GPL-2.0+
  * License URI: http://www.gnu.org/licenses/old-licenses/gpl-2.0.html
  * Text Domain: genesis-enews-extended
  * Domain Path: /languages/
